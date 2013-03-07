984055127073c8e4d5b8e3dfdb1a39819118c43e
Michael Chang <thenewme91@gmail.com>
2013-03-07 23:10:41 +0000

    Add a sample secrets file.

---
+++ secrets-sample.py
@@ -0,0 +1,5 @@
+KHANBUGZ_GITHUB_CLIENT_ID = "xxxxxxxxxxxxxxxxxxxx"
+KHANBUGZ_GITHUB_CLIENT_SECRET = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+KHANBUGZ_GITHUB_USER = "xxxxxxxx"
+KHANBUGZ_GITHUB_PASS = "xxxxxxxxxxxxxx"
+
