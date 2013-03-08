1d93307bfa8d083806b995c4dad0a528db0dbe62
Michael Chang <thenewme91@gmail.com>
2013-03-08 21:59:19 +0000

    Add some new message types to kbb.py

--- kbb.py
+++ kbb.py
@@ -28,6 +28,15 @@ def change_issue_state(owner, repo, number, state):
     r = requests.patch(uri, headers=auth_headers(), data=json.dumps(data))
     print_pretty_json(r.json())
 
+def create_comment(owner, repo, number, body):
+    uri = "https://api.github.com/repos/%s/%s/issues/%s/comments" % (owner, repo, number)
+    print uri
+    data = {
+        "body": body
+    }
+    r = requests.post(uri, headers=auth_headers(), data=json.dumps(data))
+    print_pretty_json(r.json())
+
 @app.route("/")
 def hello():
   return "Hello, World!"
@@ -49,14 +58,43 @@ def callback():
       u"message": "OK"
   }
 
+  # bail out early if the message is from us
+  # TODO(cbhl): refactor this early bail out
+  if request.json["comment"]["user"]["login"] == "KhanBugz":
+    print "It's just us, silly!"
+    return json.dumps(result)
+
   owner = request.json["repository"]["owner"]["login"]
   repo = request.json["repository"]["name"]
   number = request.json["issue"]["number"]
 
-  if ("notabug" in request.json["comment"]["body"]):
+  if ("fixedit" in request.json["comment"]["body"]):
+      create_comment(owner, repo, number,
+          """
+We think the problem you reported is fixed.
+
+If you're still having trouble, please let us know.
+
+KhanBugz - Your Friendly Khan Academy Problem Robot""")
+      change_issue_state(owner, repo, number, "closed")
+  elif ("dupeof" in request.json["comment"]["body"]):
+      create_comment(owner, repo, number,
+          """
+Thanks for reporting a problem on Khan Academy. We think you have a problem that is similar to one that someone else reported, and we ask that you look at their report to keep track of updates to this problem.
+
+Please continue to report problems that you experience on the site, as we use the number of reports to prioritize which exercises to investigate first. Thanks!
+
+KhanBugz - Your Friendly Khan Academy Problem Robot""")
+      change_issue_state(owner, repo, number, "closed")
+  elif ("notabug" in request.json["comment"]["body"]):
       if ("open" in request.json["issue"]["state"]):
           change_issue_state(owner, repo, number, "closed")
   elif ("realbug" in request.json["comment"]["body"]):
+      create_comment(owner, repo, number,
+          """
+One of our volunteers or employees will be looking at your problem soon. Hang tight!
+
+KhanBugz - Your Friendly Khan Academy Problem Robot""")
       if ("closed" in request.json["issue"]["state"]):
           change_issue_state(owner, repo, number, "open")
 
